051c6a4a4f9961c31597c5a622da7bcf7385b0b6
cwojtak <connor.wojtak@gmail.com>
2024-02-28 13:13:01 -0600

    Minor printing change

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ device = ("cuda"
 
 print("Device selected: %s" % device)
 if device == "cuda":
-    print("CUDA device selected: %s" % torch.cuda.current_device())
-    print("CUDA device count: %s" % torch.cuda.device_count())
-    print("Friendly CUDA device name: %s" % torch.cuda.get_device_name(torch.cuda.current_device()))
+    print("CUDA device name: %s" % torch.cuda.get_device_name(torch.cuda.current_device()))
 
 train_basic_model(device)
